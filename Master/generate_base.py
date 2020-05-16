fd7df747a1d1daa5ad20b80ac187a3f1e2cac385
Andrea Parisi <andreaparisi.physics@gmail.com>
2020-05-16 11:58:46 +0100

    Updated commit, missing removal of files from previous

--- Master/generate_base.py
+++
@@ -1,5 +0,0 @@
-simList = {}
-#simList['Kenya'] = {'cores': 12, 'gridres': 5, 'mobility': 0.870, 'simLen': 365}
-simList['Kenya'] = {'cores': 12, 'gridres': 5, 'mobility': 0.570, 'simLen': 365}
-simList['Italy'] = {'cores': 12, 'gridres': 5, 'mobility': 0.395, 'simLen': 365}
-
