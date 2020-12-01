bba13419af87c6b1ac1dbee0d3321aa143206cb3
Andrea Parisi <andreaparisi.physics@gmail.com>
2020-12-01 19:26:19 +0000

    Wrong engine version

--- Extern/install.sh
+++ Extern/install.sh
@@ -1,13 +1,13 @@
 if [ "$#" -eq 0 ]; then
-	wget -N https://www.deryase.org/Downloads/Installer/deryaSE_installer_rel_58.bin
+	wget -N https://www.deryase.org/Downloads/Installer/deryaSE_installer_rel_59.bin
 elif [ "$1" == "--update" ]; then
-	\rm -f deryaSE_installer_rel_58.bin
-	wget https://www.deryase.org/Downloads/Installer/deryaSE_installer_rel_58.bin
+	\rm -f deryaSE_installer_rel_59.bin
+	wget https://www.deryase.org/Downloads/Installer/deryaSE_installer_rel_59.bin
 else
 	echo "Unknown parameter [$1]"
 	exit 1
 fi
 
-chmod 0755 deryaSE_installer_rel_58.bin
-./deryaSE_installer_rel_58.bin --acceptLicence --local
+chmod 0755 deryaSE_installer_rel_59.bin
+./deryaSE_installer_rel_59.bin --acceptLicence --local
 
